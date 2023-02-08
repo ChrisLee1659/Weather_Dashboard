9fea2ffa3eee0e492f9f218b533688237d4f9d5b
ChrisLee1659 <119467134+ChrisLee1659@users.noreply.github.com>
2023-02-07 19:50:36 -0600

    adding to javascript function

--- js.js
+++ js.js
@@ -2,6 +2,11 @@ javascript
 let weather = {
     "apiKey": "4adc862ea7f2d02242ef820b53d9a380",
     fetchWeather: function (){
-      fetch ("")  
+      fetch ("api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4adc862ea7f2d02242ef820b53d9a380")
+      .then((Response) => Response.json())
+      .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+      
     }
 }
